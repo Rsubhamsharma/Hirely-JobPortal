daf7328e369d34869d567cefb6b2833faac5a6b5
Subham Sharma <rssubham63@gmail.com>
2026-01-15 19:35:09 +0530

    FIxed mongodb issue

--- Backend/src/DB/connectDB.js
+++ Backend/src/DB/connectDB.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.mongoDB_URI}/${DB_NAME}`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
     console.log(`Database connected to L: ${DB_NAME}`);
   } catch (err) {
     console.log("Error in DB connection", err.message);
